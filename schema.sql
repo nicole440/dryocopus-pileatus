6ee7112ea3f8734ec8e2674bab944b094b2aea34
Nicole <111709554+nicole440@users.noreply.github.com>
2023-06-08 17:37:55 -0400

    Update schema to change Location_ID data type to Integer, and add varchar lengths.

--- schema.sql
+++ schema.sql
@@ -2,14 +2,14 @@ DROP TABLE IF EXISTS Recent_Observations;
 
 CREATE TABLE Recent_Observations (
 	Index SERIAL PRIMARY KEY,
-	Species_Code VARCHAR,
-    Common_Name VARCHAR,
-    Scientific_Name VARCHAR,
-    Location_ID VARCHAR,
-    Location_Name VARCHAR,
+	Species_Code VARCHAR(10),
+    Common_Name VARCHAR(30),
+    Scientific_Name VARCHAR(50),
+    Location_ID INTEGER,
+    Location_Name VARCHAR(50),
     Observation_Date DATE,
     Quantity_Observed INTEGER,
-    Latitude VARCHAR,
-    Longitude VARCHAR,
+    Latitude VARCHAR(15),
+    Longitude VARCHAR(15),
     Location_Private BOOLEAN
 );
