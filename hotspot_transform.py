a84596039a0d4869ec221a6f6c91dbd14f147690
Nicole <111709554+nicole440@users.noreply.github.com>
2023-08-17 20:24:30 -0400

    Add function to transform columns for individual location info

--- hotspot_transform.py
+++ hotspot_transform.py
@@ -8,3 +8,13 @@ def transform_columns(dataframe):
                                                  'lat': 'latitude', 'lng': 'longitude', 'latestObsDt': 'date_recent_observation', 
                                                  'numSpeciesAllTime': 'total_species_recorded_at_location'})
     return dataframe
+
+
+def transform_single_location_data(dataframe):
+     # Specify the columns to be dropped
+    columns_to_drop = ['countryCode', 'countryName', 'subnational1Name', 'subnational1Code', 'locName', 'lat',	'lng', 'hierarchicalName', 'locID']
+    # Drop the specified columns from the DataFrame
+    dataframe = dataframe.drop(columns=columns_to_drop)
+    dataframe = dataframe.rename(columns={'locId': 'location_id', 'name': 'location_name', 'subnational2Code': 'country_state_county_code',
+                                                'subnational2Name': 'county_name', 'isHotspot': 'is_hotspot'})
+    return dataframe
