dd28a7775a47df625e45393d8f6836063131fb8f
Nicole <111709554+nicole440@users.noreply.github.com>
2023-08-22 08:54:59 -0400

    Refactor stored procedure to also consider observation date and common name when comparing duplicate rows. This prevents deletion of rows where multiple species observations are recorded under the same submission id.

--- RemoveDuplicates.sql
+++ RemoveDuplicates.sql
@@ -1,17 +1,19 @@
 CREATE OR REPLACE FUNCTION remove_duplicates()
-RETURNS void AS $$ --Start delimiter for the function definition
+RETURNS void AS 
+$$
 BEGIN
-    -- Create temporary table to hold unique observations
+    -- Create: temporary table to hold unique observations
     CREATE TEMP TABLE temp_unique_observations AS
-    SELECT DISTINCT ON (submission_id) *
+    SELECT DISTINCT ON (submission_id, observation_date, common_name) *
     FROM recent_observations
-    ORDER BY submission_id, observation_date DESC;
+    ORDER BY observation_date, common_name DESC;
 
-    -- Clear original table and re-insert unique observations
+    -- Clear: original table and re-insert unique observations
     DELETE FROM recent_observations;
     INSERT INTO recent_observations SELECT * FROM temp_unique_observations;
 
     -- Clean up: drop the temporary table
     DROP TABLE temp_unique_observations;
 END;
-$$ LANGUAGE plpgsql; -- End function delimiter and language declaration
+$$
+LANGUAGE plpgsql;
