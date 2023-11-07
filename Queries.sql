2e687e358b0150bd4283f3e0254e37d221d4907e
Nicole <111709554+nicole440@users.noreply.github.com>
2023-11-07 18:54:33 -0500

    Add frequently used queries

--- Queries.sql
+++ Queries.sql
@@ -1,17 +1,22 @@
 SELECT remove_duplicates();
 
 SELECT *
-FROM recent_observations
-WHERE common_name LIKE 'Pileated Woodpecker';
+FROM recent_observations;
 
-SELECT common_name, COUNT(common_name), submission_id
-FROM recent_observations
-GROUP BY recent_observations.submission_id, recent_observations.common_name;
-
-SELECT common_name, COUNT(submission_id)
+SELECT *
 FROM recent_observations
 WHERE common_name LIKE 'Pileated Woodpecker'
-GROUP BY submission_id, recent_observations.common_name;
+ORDER BY submission_id;
+
+SELECT common_name, COUNT(common_name), submission_id
+FROM recent_observations AS recent
+GROUP BY recent.submission_id, recent.common_name
+ORDER BY COUNT(common_name) DESC;
+
+SELECT common_name, COUNT(submission_id)
+FROM recent_observations AS recent
+WHERE common_name LIKE 'Pileated Woodpecker'
+GROUP BY submission_id, recent.common_name;
 
 SELECT *
 FROM recent_observations
